fc25cd1573ab7bbbd08b7f2d4e2282150b082ba3
macneib <macneib@gmail.com>
2021-02-14 23:24:47 -0500

    readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Blair MacNeil`,
+      summary: `who lives and works in Toronto.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    siteUrl: `https://macneib.github.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `macneib`,
     },
   },
   plugins: [
